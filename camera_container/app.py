8e3d6985b02f03560bd05eeac481d413f150c2e4
Luciano Felix <22255332+FelixLuciano@users.noreply.github.com>
2024-11-13 15:05:52 -0300

    Revert refactor

--- camera_container/app.py
+++ camera_container/app.py
@@ -1,52 +1,45 @@
+from flask import Flask, jsonify, abort, send_file
+import os
 import time
-from pathlib import Path
 from threading import Lock
 
-from flask import Flask, jsonify, abort, send_file
-
-DATA_DIR = Path("data")
-MOCK_DIR = DATA_DIR / "mock"
-
 app = Flask(__name__)
+
+MOCK_DIR = './mock'
 lock = Lock()
 
 def get_json_file_path(month, day=None):
-    file_path = MOCK_DIR / f"month_{month}"
-
     if day:
-        filepath = filepath / f"day_{day}.json"
+        return os.path.join(MOCK_DIR, f'month_{month}', f'day_{day}.json')
+    return os.path.join(MOCK_DIR, f'month_{month}.json')
 
-    return file_path.with_suffix("json")
-
-@app.route("/month/<int:month>", methods=["GET"])
-def get_month_data(month: int):
+@app.route('/month/<int:month>', methods=['GET'])
+def get_month_data(month):
     with lock:
-        time.sleep(10)
-
+        time.sleep(10)  # Adiciona o delay de 10 segundos
         if month < 1 or month > 12:
             abort(400, description="Bad Request")
 
         file_path = get_json_file_path(month)
 
-        if not file_path.exists():
+        if not os.path.exists(file_path):
             abort(404, description="File not found")
         
-        return send_file(file_path, mimetype="application/json")
+        return send_file(file_path, mimetype='application/json')
 
-@app.route("/month/<int:month>/day/<int:day>", methods=["GET"])
-def get_day_data(month: int, day: int):
+@app.route('/month/<int:month>/day/<int:day>', methods=['GET'])
+def get_day_data(month, day):
     with lock:
-        time.sleep(10)
-
+        time.sleep(10)  # Adiciona o delay de 10 segundos
         if month < 1 or month > 12 or day < 1 or day > 31:
             abort(400, description="Bad Request")
 
         file_path = get_json_file_path(month, day)
 
-        if not file_path.exists():
+        if not os.path.exists(file_path):
             abort(404, description="File not found")
 
-        return send_file(file_path, mimetype="application/json")
+        return send_file(file_path, mimetype='application/json')
 
 @app.errorhandler(404)
 def not_found(error):
@@ -54,11 +47,8 @@ def not_found(error):
         "error": "Not Found",
         "message": error.description
     })
-
     response.status_code = 404
-
     return response
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
