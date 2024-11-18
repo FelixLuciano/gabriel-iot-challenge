8d12fb4f71d0df6c2693f9804c78f8860c4a4a8f
Luciano Felix <22255332+FelixLuciano@users.noreply.github.com>
2024-11-18 20:12:36 -0300

    Add session config and close

--- client_container/connections/database_connection.py
+++ client_container/connections/database_connection.py
@@ -5,14 +5,20 @@ from sqlalchemy.orm import Session
 
 
 class DatabaseConnection:
+    SESSION_CONFIG = {
+        "expire_on_commit": False,
+    }
+
     def __init__(self, context: Flask):
         self.context = context
 
     @contextmanager
     def get_session(self):
         try:
-            with Session(self.context.db) as session:
+            with Session(self.context.db, **DatabaseConnection.SESSION_CONFIG) as session:
                 yield session
         except Exception as error:
-            self.context.logger.fatal(error)
+            self.context.logger.fatal(error, error.args)
             session.rollback()
+        finally:
+            session.close()
