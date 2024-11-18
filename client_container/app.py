82589fba5fb6ada3bf0c230e31c48da47d3a9ad2
Luciano Felix <22255332+FelixLuciano@users.noreply.github.com>
2024-11-18 20:14:50 -0300

    Add resource routes, fix init db and missing entities

--- client_container/app.py
+++ client_container/app.py
@@ -1,13 +1,13 @@
-import logging
+# import logging
 
 from flask import Flask, g, jsonify
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 
 from connections import DatabaseConnection
 from constants import DB_SERVICE_URL
 from errors import BaseError
 from models import BaseModel, DiscEventModel, RecordTypeModel
-from resources import Record
+from resources import Records
 
 
 # logging.basicConfig(format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
@@ -16,9 +16,14 @@ from resources import Record
 
 app = Flask(__name__)
 
-app.db = create_engine(DB_SERVICE_URL, echo=True)
+app.db = create_engine(DB_SERVICE_URL, echo=False)
 
-app.add_url_rule("/record/<int:month>", view_func=Record.as_view("record", app))
+records_resource = Records(app)
+
+app.add_url_rule("/records/", view_func=records_resource.get_by_query, methods=["GET"])
+app.add_url_rule("/records/year/<int:year>", view_func=Records.as_view("RecordsOnYear", app))
+app.add_url_rule("/records/year/<int:year>/month/<int:month>", view_func=Records.as_view("RecordsOnMonth", app))
+app.add_url_rule("/records/year/<int:year>/month/<int:month>/day/<int:day>", view_func=Records.as_view("RecordsOnDay", app))
 
 @app.errorhandler(404)
 def not_found(error):
@@ -52,13 +57,17 @@ def close_connection(exception):
     db.close()
 
 def init_db():
+    BaseModel.metadata.drop_all(app.db)
     BaseModel.metadata.create_all(app.db)
 
     models = [
         DiscEventModel.create(0, "EVENT0"),
         DiscEventModel.create(1, "EVENT1"),
         DiscEventModel.create(2, "EVENT2"),
+        DiscEventModel.create(3, "EVENT3"),
         DiscEventModel.create(4, "EVENT4"),
+        DiscEventModel.create(5, "EVENT5"),
+        DiscEventModel.create(6, "EVENT6"),
         RecordTypeModel.create(1, "NormalRecord"),
         RecordTypeModel.create(2, "AlarmRecord"),
         RecordTypeModel.create(4, "MotionRecord"),
@@ -67,8 +76,6 @@ def init_db():
     with DatabaseConnection(app).get_session() as session:
         session.add_all(models)
         session.commit()
-
-    # app.db.raw_connection().cursor().executescript(open("schema.sql").read())
 
 init_db()
 
